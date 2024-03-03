afeb6eb75362c5cfe6a367f815a79bf40479df47
hailey <lillyine.hy@gmail.com>
2024-03-03 23:04:30 +0900

    feat: 최댓값과 최솟값 - 프로그래머스 LV.2

---
+++ MAR-1/programmers_12939.py
@@ -0,0 +1,7 @@
+# 최댓값과 최솟값 
+
+function solution(s) {
+    const array = s.split(' ').map(elem => parseInt(elem))
+    const answer = new Array(Math.min(...array), Math.max(...array))
+    return answer.join(' ')
+}
