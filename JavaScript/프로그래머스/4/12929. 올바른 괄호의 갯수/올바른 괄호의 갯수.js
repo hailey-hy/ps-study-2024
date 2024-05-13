1a825ae676c059d76583486cedeb07b3dd5dbc54
전해연 <93465128+hailey-hy@users.noreply.github.com>
2024-05-13 19:25:18 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.js
+++ JavaScript/프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.js
@@ -1,10 +1,11 @@
 function solution (n) {
+    
   const dp = new Array(n+1).fill(0);
   dp[0] = dp[1] = 1;
   
   for(let i = 2; i <= n; i++) {
     for(let j = 1; j <= i; j++) {
-      dp[i] += dp[j-1] * dp[i-j];
+      dp[i] += dp[j - 1] * dp[i - j];
     }
   }
   
