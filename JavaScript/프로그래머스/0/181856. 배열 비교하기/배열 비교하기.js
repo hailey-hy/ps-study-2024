648e0f0ebd7d0cc6319a17aa3232a7ef2a6b1863
전해연 <93465128+hailey-hy@users.noreply.github.com>
2025-02-27 14:55:26 +0900

    [level 0] Title: 배열 비교하기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
+++ JavaScript/프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -1,12 +1,12 @@
 function solution(arr1, arr2) {
-    if(arr1.length != arr2.length) {
-        return arr1.length > arr2.length ? 1 : -1
-    }
-    else {
-        sum1 = arr1.reduce((acc, cur) => acc + cur, 0);
-        sum2 = arr2.reduce((acc, cur) => acc + cur, 0);
-        if(sum1 > sum2) return 1
-        else if(sum1 < sum2) return -1
-        else return 0
-    }
+    if(arr1.length != arr2.length) return arr1.length > arr2.length ? 1 : -1
+    
+    sum1 = arr1.reduce((acc, cur) => acc + cur, 0);
+    sum2 = arr2.reduce((acc, cur) => acc + cur, 0);
+    
+    return sum1 >= sum2 ? sum1 === sum2 ? 0 : 1 : -1
+    // if(sum1 > sum2) return 1
+    // else if(sum1 < sum2) return -1
+    // else return 0
+    
 }
