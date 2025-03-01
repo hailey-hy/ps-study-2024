0bd7c3dbd48927cf09155a466f4fa79f0d2f0a0d
전해연 <93465128+hailey-hy@users.noreply.github.com>
2025-03-01 13:46:35 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ JavaScript/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,29 +1,10 @@
-const words = {
-    'zero' : '0',
-    'one' : '1',
-    'two' : '2',
-    'three' : '3', 
-    'four' : '4', 
-    'five' : '5', 
-    'six' : '6', 
-    'seven' : '7',
-    'eight' : '8',
-    'nine' : '9'
-}
+const words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
 function solution(s) {
-    let answer = '';
-    let temp = '';
-    [...s].map((value) => {
-        if (Number.isFinite(Number(value))) {
-           answer += value; 
-        } else {
-            temp += value;
-            if(temp in words) {
-                answer += words[temp];
-                temp = '';
-            }
-        }
-    })
+    let answer = s;
+    for (i = 0; i < 10; i++) {
+        const splited = answer.split(words[i]);
+        answer = splited.join(`${i}`);
+    }
     return Number(answer);
 }
