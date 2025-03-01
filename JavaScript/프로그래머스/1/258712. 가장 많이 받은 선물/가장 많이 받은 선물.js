b5185738190b14b65c8b6ec72b4e02bcfccf9955
전해연 <93465128+hailey-hy@users.noreply.github.com>
2025-03-01 21:54:27 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 29.25 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -0,0 +1,40 @@
+function solution(friends, gifts) {
+    const gift_index = Array(friends.length).fill(0);
+    const matrix = Array.from({length: friends.length}, () => Array(friends.length).fill(0));
+    const answer = Array(friends.length).fill(0);
+    
+    gifts.forEach((gift) => {
+        const [ giver, taker ] = gift.split(' ');
+        const i = friends.indexOf(giver);
+        const j = friends.indexOf(taker);
+        
+        matrix[i][j] += 1;
+        gift_index[i] += 1;
+        gift_index[j] -= 1;
+    })
+    
+    for (i = 0; i < friends.length; i++) {
+        for (j = 0; j < friends.length; j++) {
+            if(i >= j) continue;
+            if(matrix[i][j] === matrix[j][i]) { //선물 지수 비교
+                if(gift_index[i] > gift_index[j]) {
+                    answer[i] += 1;
+                } else if (gift_index[i] < gift_index[j]) {
+                    answer[j] += 1;
+                }
+                console.log(`${friends[j]}가 ${friends[i]} 에게, ${answer}, i = ${i}, j = ${j} !` );
+            } else {
+                if(matrix[i][j] > matrix[j][i]) {
+                    answer[i] += 1;
+                } else {
+                    answer[j] += 1;
+                }
+                console.log(`${friends[j]}가 ${friends[i]} 에게, ${answer}, i = ${i}, j = ${j}`);
+            }
+        }
+    }
+    return Math.max(...answer)
+}
+
+
+// 
